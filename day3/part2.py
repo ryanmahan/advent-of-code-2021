774ef9a02888677924196bb9ab2d9aa0f4041604
Ryan Mahan <ryanmahan97@gmail.com>
2021-12-16 17:47:14 -0500

    no need to read file twice

--- day3/part2.py
+++ day3/part2.py
@@ -1,9 +1,6 @@
-from copy import deepcopy
-
-def filter_by_bit(lines: [str], position: int, most_often: bool):
+def filter_by_bit(lines: list[str], position: int, most_often: bool):
   bits = list(map(lambda x: int(x[position]), lines))
   filter_bit = "0"
-  print(sum(bits) >= len(lines) / 2)
   if (sum(bits) >= len(lines) / 2) is most_often:
     filter_bit = "1"
   return list(filter(lambda x: x[position] == filter_bit, lines))
@@ -11,15 +8,15 @@ def filter_by_bit(lines: [str], position: int, most_often: bool):
 with open("data.txt", "r") as file:
   lines = file.readlines()
   oxygen = lines
+  carbon = lines
+
   for i in range(0, len(lines[0]) - 1):
     oxygen = filter_by_bit(oxygen, i, True)
-  
   print(oxygen)
 
-with open("data.txt", "r") as file:
-  lines = file.readlines()
-  carbon = lines
   for i in range(0, len(lines[0]) - 1):
       carbon = filter_by_bit(carbon, i, False)
+      if (len(carbon) == 1):
+        break
   print(carbon)
     
