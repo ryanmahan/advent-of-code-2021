ba700a674563daafe02c59e834055e01e519ec49
Ryan Mahan <ryanmahan97@gmail.com>
2021-12-19 12:14:13 -0500

    solved a few more days

---
+++ day7/part1.cpp
@@ -0,0 +1,55 @@
+#include <vector>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <numeric>
+#include <algorithm>
+#include <limits>
+
+int get_fuel_cost(std::vector<int> positions, int end_point) {
+  std::vector<int> fuel_cost(positions.size(), 0);
+  std::transform(positions.begin(), positions.end(), fuel_cost.begin(), [end_point](int pos){ return std::abs(end_point - pos); });
+  return std::accumulate(fuel_cost.begin(), fuel_cost.end(), 0);
+}
+
+int get_fuel_cost_part2(std::vector<int> positions, int end_point) {
+  std::vector<int> fuel_cost(positions.size(), 0);
+  std::transform(positions.begin(), positions.end(), fuel_cost.begin(), [end_point](int pos){ 
+    int dist = std::abs(end_point - pos); 
+    return dist * (dist+1) / 2;
+  });
+  return std::accumulate(fuel_cost.begin(), fuel_cost.end(), 0);
+}
+
+int main() {
+  std::ifstream file("data.txt");
+  std::string line;
+  std::getline(file, line);
+  std::stringstream stream(line);
+  std::vector<int> positions;
+  int h_pos;
+  while(stream >> h_pos) {
+    // std::cout << h_pos << " ";
+    positions.push_back(h_pos);
+    if (stream.peek() == ',')
+      stream.ignore();
+  }
+  std::cout << positions.size();
+  int max = *std::max_element(std::begin(positions), std::end(positions));
+  int min = *std::min_element(std::begin(positions), std::end(positions));
+  int current = 0, lowest = std::numeric_limits<int>::max(), ideal = -1;
+  std::cout << "min " << min << " max " << max << "\n";
+  // sad brute force method
+  for (int i = max ; i > min ; i--) {
+    current = get_fuel_cost_part2(positions, i);
+    if (current <= lowest) {
+      lowest = current;
+      ideal = i;
+      // std::cout << lowest << " " << current << " " << i << "\n";
+    }
+    std::cout << lowest << " " << current << " " << i << "\n";
+  }
+  std::cout << lowest << ideal;
+  
+}
